83dd8a0db19982ccfad04556b44ff7a8cb85b21c
cjw20 <cjw20@iu.edu>
2020-09-24 12:20:08 -0400

    added interaction check for customer as well

--- Midnight Customers Prototype 1/Assets/Scripts/PlayerController.cs
+++ Midnight Customers Prototype 1/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,9 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public GameObject currentInteraction;  //object player is currently interacting with
 
+    bool nearCustomer;
+    public GameObject closestCustomer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,10 +28,16 @@ public class PlayerController : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.E))
         {
-            if(currentInteraction != null)
+            if (nearCustomer)
+            {
+
+            }
+            
+            else if(currentInteraction != null)   //prioritizes customers over items, may need to change how this works later
             {
                 //interact
             }
+           
         }
     }
 
@@ -43,10 +52,27 @@ public class PlayerController : MonoBehaviour
         {
             currentInteraction = other.gameObject;
         }
+
+        if(other.tag == "Customer")
+        {
+            nearCustomer = true;
+            closestCustomer = other.gameObject;
+
+        }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D other)
     {
-        currentInteraction = null;
+
+        if (other.tag == "Interactable")
+        {
+            currentInteraction = null;
+        }
+        if (other.tag == "Customer")
+        {
+            nearCustomer = false;
+            closestCustomer = null;
+
+        }
     }
 }
