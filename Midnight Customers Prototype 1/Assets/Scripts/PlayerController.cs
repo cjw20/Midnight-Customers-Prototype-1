0888c728862f2784dad0ad3616ddeb18eb9c2f40
cjw20 <cjw20@iu.edu>
2020-09-24 12:11:24 -0400

    Added colliders to detect if player is in range of interactable

--- Midnight Customers Prototype 1/Assets/Scripts/PlayerController.cs
+++ Midnight Customers Prototype 1/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     Vector2 movement;
     public Rigidbody2D body;
     public float moveSpeed;
+    public GameObject currentInteraction;  //object player is currently interacting with
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,10 +22,31 @@ public class PlayerController : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical"); //movement using wasd or arrow keys
 
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if(currentInteraction != null)
+            {
+                //interact
+            }
+        }
     }
 
     void FixedUpdate()
     {
         body.MovePosition(body.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Interactable")
+        {
+            currentInteraction = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        currentInteraction = null;
     }
 }
