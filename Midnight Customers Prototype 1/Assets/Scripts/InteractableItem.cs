d244b26a72b9346765bcf39607ac94cda99030dc
cjw20 <cjw20@iu.edu>
2020-09-24 12:53:51 -0400

    Added goo object

--- Midnight Customers Prototype 1/Assets/Scripts/InteractableItem.cs
+++ Midnight Customers Prototype 1/Assets/Scripts/InteractableItem.cs
@@ -7,6 +7,9 @@ public class InteractableItem : MonoBehaviour
     public string itemName; //name of item that will show up in inventory/ UI
     public bool canHold; //true if item can be picked up and put into player inventory
     public int amount; //amount of the item that the player will pickup 
+    public bool isMiniGameTrigger; //if interacting with this will trigger minigame
+    public bool hasPrerequisite; //if interaction requires something from the player in order for it to occur properly
+    public string prerequisite; //name of requirement for interaction ex. Mop
 
     // Start is called before the first frame update
     void Start()
