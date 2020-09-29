de37bb0e6c9d3292b46c5dc1f94a36fb258ca77a
cjw20 <cjw20@iu.edu>
2020-09-29 14:15:38 -0400

    made customer wait at destination for a while before moving to next one

--- Midnight Customers Prototype 1/Assets/Scripts/CustomerMovement.cs
+++ Midnight Customers Prototype 1/Assets/Scripts/CustomerMovement.cs
@@ -9,12 +9,13 @@ public class CustomerMovement : MonoBehaviour
     
     NavMeshAgent2D agent;
 
-    public float speed = 5f;
+   // public float speed = 5f;
     public float minDistance = 0.5f;
     public GameObject[] waypoints;
     public int destination = 0;
     public Transform target;
-
+    public float timeToWait = 5f; //time for customer to chill at destination before moving to next one
+    bool isWaiting;
 
     void Start()
     {
@@ -26,9 +27,12 @@ public class CustomerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isWaiting)
+            return;
 
         if (!agent.pathPending && agent.remainingDistance < minDistance)
-            GoToNextPoint();
+            StartCoroutine("wait");
+            
     }
     void GoToNextPoint()
     {
@@ -37,5 +41,14 @@ public class CustomerMovement : MonoBehaviour
         agent.destination = waypoints[destination].transform.position;
 
         destination = (destination + 1) % waypoints.Length;
+    }
+
+    IEnumerator wait()
+    {
+        isWaiting = true;
+        yield return new WaitForSeconds(timeToWait);
+        isWaiting = false;
+        GoToNextPoint();
+        yield break;
     }
 }
