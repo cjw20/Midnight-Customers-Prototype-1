1007b09ed7f2996361c4744eeadcb0b87aa5cb24
cjw20 <cjw20@iu.edu>
2020-09-29 14:08:45 -0400

    Made customer wander around store

--- Midnight Customers Prototype 1/Assets/Scripts/CustomerMovement.cs
+++ Midnight Customers Prototype 1/Assets/Scripts/CustomerMovement.cs
@@ -5,20 +5,37 @@ using UnityEngine;
 public class CustomerMovement : MonoBehaviour
 {
     
-    public Rigidbody2D body;
-    public float moveSpeed;
+    //public Rigidbody2D body;
+    
     NavMeshAgent2D agent;
+
+    public float speed = 5f;
+    public float minDistance = 0.5f;
+    public GameObject[] waypoints;
+    public int destination = 0;
+    public Transform target;
 
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent2D>();
-
+        waypoints = GameObject.FindGameObjectsWithTag("Destination");
+        GoToNextPoint();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (!agent.pathPending && agent.remainingDistance < minDistance)
+            GoToNextPoint();
+    }
+    void GoToNextPoint()
+    {
         
+
+        agent.destination = waypoints[destination].transform.position;
+
+        destination = (destination + 1) % waypoints.Length;
     }
 }
