b77f4dbf751f62c7e14f3208045aaf99fead33c0
cjw20 <cjw20@iu.edu>
2020-09-29 17:18:53 -0400

    started work on dialogue system

---
+++ Midnight Customers Prototype 1/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue : MonoBehaviour
+{
+    
+    public string nameText; //name of speaker
+
+    [TextArea(3, 10)]
+    public string[] sentences;
+
+    public GameObject[] options;
+}
---
+++ Midnight Customers Prototype 1/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    Queue<string> sentences;
+    public Text nameText;
+    public Text dialogueText;
+    public GameObject dialogueWindow;
+
+    public GameObject[] dialogueOptions;
+
+    public GameObject optionsWindow;
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+    public void StartDialogue(Dialogue dialogue)
+    {
+        sentences.Clear();
+        Time.timeScale = 0; //pauses updates for objects in scene
+
+        dialogueOptions = dialogue.options;
+
+        dialogueWindow.SetActive(true);
+
+        nameText.text = dialogue.nameText;
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            if(dialogueOptions.Length == 0)
+            {
+                EndDialogue();
+
+                return;
+            }
+            
+            
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+
+    }
+
+    void EndDialogue()
+    {
+        dialogueWindow.SetActive(false);
+
+        Time.timeScale = 1f;
+
+    }
+}
