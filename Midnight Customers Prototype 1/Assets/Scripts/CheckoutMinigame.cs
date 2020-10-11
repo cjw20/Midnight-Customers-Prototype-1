836ad25dc6ef4a23fb0b742e59afc65631547d35
cjw20 <cjw20@iu.edu>
2020-10-11 16:46:43 -0400

    began work on checkout minigame

---
+++ Midnight Customers Prototype 1/Assets/Scripts/CheckoutMinigame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckoutMinigame : MonoBehaviour
+{
+    public GameObject[] itemSpawns; //where check out items will appear\
+    public GameObject checkoutItem; //item that will be checked out. Can add variation later 
+
+    int itemNumber; //number of items being checked out
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(GameObject itemspawn in itemSpawns)
+        {
+            Instantiate(checkoutItem, itemspawn.transform);
+            itemNumber++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
