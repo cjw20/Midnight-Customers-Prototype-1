e96a1eacfe9d363403de1c0b7a4b5a46fb56b5bc
cjw20 <cjw20@iu.edu>
2020-10-11 18:00:37 -0400

    Completed basic version of shelf stocking minigame and put trigger for it in store

---
+++ Midnight Customers Prototype 1/Assets/Scripts/StockingMinigame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StockingMinigame : MonoBehaviour
+{
+    public GameObject[] emptySpots;
+    int numberOfSpots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numberOfSpots = emptySpots.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateCount()
+    {
+        numberOfSpots--;
+        if(numberOfSpots < 1)
+        {
+            Destroy(this.gameObject); //closes minigame after each shelf has been stocked
+        }
+    }
+}
