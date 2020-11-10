c2fc2042c84c7de9629bd2aab0f04fdfe254cd95
cjw20 <cjw20@iu.edu>
2020-11-10 13:59:00 -0500

    Created handbook for checkout. Also fixed player collision in store.
    No more spinning :(

---
+++ Midnight Customers Prototype 1/Assets/Scripts/Handbook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Handbook : MonoBehaviour
+{
+    public GameObject openBook;
+    bool open = false; //false if closed
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        open = !open;
+        openBook.SetActive(open);
+        
+    }
+
+   
+}
