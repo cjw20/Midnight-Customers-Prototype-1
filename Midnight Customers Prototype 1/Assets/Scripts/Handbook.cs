6f61251f919140930bffa412eec533c5e15dd2c8
cjw20 <cjw20@iu.edu>
2020-11-19 15:14:05 -0500

    Playable Prototype

--- Midnight Customers Prototype 1/Assets/Scripts/Handbook.cs
+++ Midnight Customers Prototype 1/Assets/Scripts/Handbook.cs
@@ -6,17 +6,7 @@ public class Handbook : MonoBehaviour
 {
     public GameObject openBook;
     bool open = false; //false if closed
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
     private void OnMouseDown()
     {
         open = !open;
