7ceb6ae88938201bec1c0b2843157a412a91af21
cjw20 <cjw20@iu.edu>
2020-12-01 12:30:32 -0500

    made it so first customer does not come in until 20 seconds have passed

---
+++ Midnight Customers Prototype 1/Assets/Scripts/CustomerSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerSpawner : MonoBehaviour
+{
+    bool timerRunning;
+    float timeRemaining;
+    public GameObject customer;
+    void Start()
+    {
+        timerRunning = true;
+        timeRemaining = 20;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (timerRunning)
+        {
+
+            timeRemaining -= Time.fixedDeltaTime; ;
+            
+            if(timeRemaining <= 0)
+            {
+                timerRunning = false;
+                customer.SetActive(true);
+            }
+
+            
+        }
+    }
+}
